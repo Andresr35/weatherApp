888dc5c9cce4b510e2c3d01cace7b8830f6d1232
Andresr35 <andresjruiz@cpp.edu>
2023-08-20 13:41:39 -0700

    add readme and finish project

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Weather App
+
+This app just showcases using the WeatherAPI for the Odin Project with a simple form.
--- src/index.js
+++ src/index.js
@@ -7,13 +7,36 @@ const key = "581e5cf0257e42ec894184439231808";
 const fetchWeather = async (location) => {
   try {
     const res = await fetch(
-      baseUrl + `/current.json?key=${key}&q=${location}`,
+      `${baseUrl}/current.json?key=${key}&q=${location}`,
       {
         mode: "cors",
       }
     );
-    return res.json();
+
+    return await res.json();
   } catch (error) {
-    console.log(error);
+    return { status: "Error" };
   }
 };
+
+const locationForm = document.querySelector("form");
+const main = document.querySelector("main");
+const container = main.appendChild(document.createElement("div"));
+container.id = "container";
+
+locationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchWeather(e.target[0].value).then((res) => {
+    container.replaceChildren();
+    Object.keys(res).forEach((value) => {
+      const div = container.appendChild(document.createElement("div"));
+      div.innerText = value;
+      Object.keys(res[value]).forEach((sVal) => {
+        const innerDiv = div.appendChild(document.createElement("div"));
+        innerDiv.innerText = sVal.concat(":");
+        const innerSpan = innerDiv.appendChild(document.createElement("span"));
+        innerSpan.innerText = res[value][sVal];
+      });
+    });
+  });
+});
